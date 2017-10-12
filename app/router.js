a969bfd350543df517a9db49f014ae6b19932819
Max Van Lyl <maxim@freshbooks.com>
2017-10-12 16:05:31 -0400

    learned nested routes

--- app/router.js
+++ app/router.js
@@ -7,6 +7,14 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('foods', function(){
+    this.route('food', {path: ':food_id'}, function() {
+      this.route('eating')
+    });
+  });
+  this.route('about');
+  this.route('contact');
+  this.route('favourite-word', {path: 'favourite-word/:word'});
 });
 
 export default Router;
